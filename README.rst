a13765023c8a199f4898ebf93536f0afcc9286ad
teo <teodantes@gmail.com>
2021-10-21 14:59:39 +0300

    Initial commit

---
+++ README.rst
@@ -0,0 +1,1 @@
+The package that express a given integer as the product of its prime factors.
---
+++ canonicalrepresentation/canrepr.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+def can_repr(x: int) -> list:
+    '''Express a given integer as the product of its prime factors.'''
+    result = []
+    if x <= 0:
+        return []
+    if x == 1:
+        return []
+    for i in range(x - 1, 1, -1):
+        if x % i == 0:
+            result += [x // i]
+            x = i
+    result += [x]
+    return result
+
+def main():
+    try:
+        a = int(input('\t\t> '))
+        print('\n\t' + 'RESULT: ', can_repr(a), '\n')
+    except:
+        print('\n\t' + 'Enter an integer!' + '\n')
+        return main()
+
+def start():
+    print('\n\t' + 'Enter an integer you want to decompose into its prime factors:' + '\n')
+    main()
+
+if __name__ == '__main__':
+    start()
